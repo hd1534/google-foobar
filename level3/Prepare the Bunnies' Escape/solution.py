fe7bf7a356f2ecbdad39671a7ee3a2c483d9211b
nahjh2 <nahjh2@google.com>
2021-02-24 19:24:30 +0900

    틀렸던 이유가 가로 세로 길이가 다를 수 가 있는거였구나 시간 초과나 재귀함수 버퍼 초과도 아닌듯

--- level3/Prepare the Bunnies' Escape/solution.py
+++ level3/Prepare the Bunnies' Escape/solution.py
@@ -1,52 +1,50 @@
-length = 0
+from collections import deque
 
-def dfs(arr, x, y, dep):
 
-    if arr[x][y] == -1:
-        return
-    if arr[x][y] <= dep:
-        return
-    arr[x][y] = dep
+def bfs(_map, x, y):
+    height = len(_map[0])
+    width = len(_map)
 
-    for x_move, y_move in [(1,0), (-1,0), (0,1), (0,-1)]:
-        dfs(arr, x + x_move, y + y_move, dep + 1)
+    arr = [[ None for _ in range(height)] for _ in range(width)]
+    arr[x][y] = 1
+
+    queue = deque([(x, y)])
+    while queue:
+        x, y = queue.pop()
+        for x_move, y_move in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            next_x, next_y = x + x_move, y + y_move
+            
+            if 0 <= next_x < width and 0 <= next_y < height and arr[next_x][next_y] is None:
+                arr[next_x][next_y] = arr[x][y] + 1
+
+                if _map[next_x][next_y] == 0:
+                    queue.appendleft((next_x, next_y))
+
+    return arr
+
 
 
 def solution(_map):
-    length = len(_map)
-    door_to_pod = [[-1 for _ in range(length + 2)] for _ in range(length + 2)]
-    pod_to_door = [[-1 for _ in range(length + 2)] for _ in range(length + 2)]
+    height = len(_map[0])
+    width = len(_map)
 
-    for i in range(length):
-        for j in range(length):
-            if _map[i][j] == 1:
-                door_to_pod[i+1][j+1] = -1
-                pod_to_door[i+1][j+1] = -1
-            else:
-                door_to_pod[i+1][j+1] = 999999999999999
-                pod_to_door[i+1][j+1] = 999999999999999
+    door_to_pod = bfs(_map, 0,0)
+    pod_to_door = bfs(_map, width - 1, height - 1)
 
-    
-    dfs(door_to_pod, 1, 1, 1)
-    dfs(pod_to_door, length, length, 1)
-    result = door_to_pod[length][length]
-
-    # print("door to pod")
+    # print("map")
+    # for i in _map:
+    #     print(i)
+    # print("\ndoor to pod")
     # for i in door_to_pod:
     #     print(i)
     # print("\npod to door")
     # for i in pod_to_door:
     #     print(i)
 
-    for i in range(1, length + 1):
-        for j in range(1, length + 1):
-            if pod_to_door[i][j] == -1:
-                if pod_to_door[i-1][j] != -1 and pod_to_door[i+1][j] != -1:
-                    result = min(result, pod_to_door[i-1][j] + door_to_pod[i+1][j] + 1, pod_to_door[i+1][j] + door_to_pod[i-1][j] + 1)
-                if pod_to_door[i][j-1] != -1 and pod_to_door[i][j+1] != -1:
-                    result = min(result, pod_to_door[i][j-1] + door_to_pod[i][j+1] + 1, pod_to_door[i][j+1] + door_to_pod[i][j-1] + 1)
+    result = float('inf')
+    for i in range(width):
+        for j in range(height):
+            if door_to_pod[i][j] and pod_to_door[i][j]:
+                result = min(result, door_to_pod[i][j] + pod_to_door[i][j] - 1)
 
     return result
-
-
-print(solution([[0, 1, 1, 0], [0, 0, 0, 1], [1, 1, 0, 0], [1, 1, 1, 0]]))
